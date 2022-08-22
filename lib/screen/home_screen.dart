bac945ea051d5367898158ab20985bb3a26db78a
JC <withj1123@gmail.com>
2022-08-22 17:06:39 +0900

    done

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,139 @@
+import 'package:calendar_scheduler/component/main_calendar.dart';
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:calendar_scheduler/provider/schedule_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<ScheduleProvider>();
+    final selectedDate = provider.selectedDate;
+    final schedules = provider.cache[selectedDate] ?? [];
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            isScrollControlled: true,
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate,
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: (selectedDate, focusedDate) =>
+                  onDaySelected(selectedDate, focusedDate, context),
+            ),
+            SizedBox(height: 8.0),
+            // DRIFT 사용시
+            // StreamBuilder<List<Schedule>>(
+            //     stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+            //     builder: (context, snapshot) {
+            //       return TodayBanner(
+            //         selectedDate: selectedDate,
+            //         count: snapshot.data?.length ?? 0,
+            //       );
+            //     }),
+            // REST API 사용시
+            TodayBanner(
+              selectedDate: selectedDate,
+              count: schedules.length,
+            ),
+            SizedBox(height: 8.0),
+            // REST API 사용시
+            Expanded(
+              child: ListView.builder(
+                itemCount: schedules.length,
+                itemBuilder: (context, index) {
+                  final schedule = schedules[index];
+
+                  return Dismissible(
+                    key: ObjectKey(schedule.id),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (DismissDirection direction) {
+                      provider.deleteSchedule(
+                          date: selectedDate, id: schedule.id);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          bottom: 8.0, left: 8.0, right: 8.0),
+                      child: ScheduleCard(
+                        startTime: schedule.startTime,
+                        endTime: schedule.endTime,
+                        content: schedule.content,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            // DRIFT 사용시
+            // Expanded(
+            //   child: StreamBuilder<List<Schedule>>(
+            //     stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+            //     builder: (context, snapshot) {
+            //       if (!snapshot.hasData) {
+            //         return Container();
+            //       }
+            //
+            //       return ListView.builder(
+            //         itemCount: snapshot.data!.length,
+            //         itemBuilder: (context, index) {
+            //           final schedule = snapshot.data![index];
+            //
+            //           return Dismissible(
+            //             key: ObjectKey(schedule.id),
+            //             direction: DismissDirection.endToStart,
+            //             onDismissed: (DismissDirection direction) {
+            //               GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
+            //             },
+            //             child: Padding(
+            //               padding: const EdgeInsets.only(
+            //                   bottom: 8.0, left: 8.0, right: 8.0),
+            //               child: ScheduleCard(
+            //                 startTime: schedule.startTime,
+            //                 endTime: schedule.endTime,
+            //                 content: schedule.content,
+            //               ),
+            //             ),
+            //           );
+            //         },
+            //       );
+            //     },
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(
+    DateTime selectedDate,
+    DateTime focusedDate,
+    BuildContext context,
+  ) {
+    final provider = context.read<ScheduleProvider>();
+    provider.changeSelectedDate(
+      date: selectedDate,
+    );
+    provider.getSchedules(date: selectedDate);
+  }
+}
